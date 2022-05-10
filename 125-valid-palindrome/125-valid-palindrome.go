16f594b95bf73fb4f4a27dbedd40eb7590642eb5
JOB MONARI <88348681+narie-monarie@users.noreply.github.com>
2022-05-10 21:28:11 +0300

    Update 125-valid-palindrome.go

--- 125-valid-palindrome/125-valid-palindrome.go
+++ 125-valid-palindrome/125-valid-palindrome.go
@@ -20,3 +20,26 @@ func isPalindrome(s string) bool {
 
 	return true
 }
+
+//or
+
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+
+	r, _ := regexp.Compile("[^a-z0-9]+")
+
+	s = r.ReplaceAllString(s, "")
+
+	start, end := 0, len(s)-1
+
+	for start < end {
+		if s[start] != s[end] {
+			return false
+		}
+
+		start++
+		end--
+	}
+
+	return true
+}

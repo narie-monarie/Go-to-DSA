72dc5efe86a1a36ad0b47fdbf5c8bcd3c980b8a8
JOB MONARI <88348681+narie-monarie@users.noreply.github.com>
2022-05-10 19:53:33 +0300

    Exercises

---
+++ Exercise/closestValue.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func alphaNumeric(c) {
+	return (int('a') <= asciCode && asciCode <= int('z')) ||
+		(int('A') <= asciCode && asciCode <= int('Z')) ||
+		(int('0') <= asciCode && asciCode <= int('9'))
+}
+
+func isPalindrome(s string) bool {
+	start, end := 0, len(s)-1
+	for start < end {
+		for start < end && !isAlphaNumeric(int(s[start])) {
+			start++
+		}
+		for start < end && !isAlphaNumeric(int(s[end])) {
+			end--
+		}
+		if strings.ToLower(string(s[start])) != strings.ToLower(string(s[end])) {
+			return false
+		}
+		start++
+		end--
+	}
+	return true
+}
+
+func main() {
+	fmt.Println()
+}

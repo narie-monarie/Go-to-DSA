ae8c07644c42f051f0c2d2b93c04d8f4dc0b76cb
JOB MONARI <88348681+narie-monarie@users.noreply.github.com>
2022-05-10 21:25:36 +0300

    Time: 8 ms (35.75%), Space: 3.1 MB (54.78%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.go
+++ 125-valid-palindrome/125-valid-palindrome.go
@@ -1,23 +1,22 @@
-func isPalindrome(s string) bool {    
-    for start, end := 0, len(s)-1; start < end; start, end = start+1,end-1 {
-        for start < end && !isAlphaNumeric(int(s[start])) {
-            start++
-        }
-        
-        for start < end && !isAlphaNumeric(int(s[end])) {
-            end--
-        }
-        
-        if strings.ToLower(string(s[start])) != strings.ToLower(string(s[end])) {
-            return false
-        }
-    }
-    
-    return true
-}
+func isPalindrome(s string) bool {
+  bs := []byte(s)
+	n := 0
+	for _, b := range bs {
+		if ('a' <= b && b <= 'z') ||
+			('A' <= b && b <= 'Z') ||
+			('0' <= b && b <= '9') {
+			bs[n] = b
+			n++
+		}
+	}
 
-func isAlphaNumeric(asciCode int) bool {
-	return (int('a') <= asciCode && asciCode <= int('z')) ||
-		(int('A') <= asciCode && asciCode <= int('Z')) ||
-		(int('0') <= asciCode && asciCode <= int('9'))
+	bsl := strings.ToLower(string(bs[:n]))
+
+	for i, j := 0, len(bsl)-1; i <= j; i, j = i+1, j-1 {
+		if bsl[i] != bsl[j] {
+			return false
+		}
+	}
+
+	return true
 }

4626855dd929329b5f3b2c8ae8b0633a24888e8a
JOB MONARI <88348681+narie-monarie@users.noreply.github.com>
2022-05-10 21:17:22 +0300

    Time: 24 ms (14.07%), Space: 3.9 MB (32.65%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.go
@@ -0,0 +1,16 @@
+func isPalindrome(s string) bool {
+	if len(s) <= 1 {
+		return true
+	}
+	reg, _ := regexp.Compile("[^a-zA-Z0-9]+")
+    s =strings.ToLower(reg.ReplaceAllString(s, ""))
+	lp, rp := 0, len(s)-1
+    for lp <= rp {
+        if s[lp] != s[rp] {
+			return false
+		}
+		lp++
+		rp--
+	}
+	return true
+}

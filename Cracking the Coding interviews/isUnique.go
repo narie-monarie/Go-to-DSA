17540d6afdb70e7c941a75ed550d47b47a440ddf
narie <monarijob@yahoo.com>
2022-05-19 02:18:56 +0300

    Check unique Characters

---
+++ Cracking the Coding interviews/isUnique.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func IsUnique(s string) bool {
+	m := make(map[rune]bool)
+
+	for _, i := range s {
+		_, ok := m[i]
+		if ok {
+			return false
+		}
+		m[i] = true
+	}
+	return true
+
+}
+
+func main() {
+	fmt.Println(IsUnique("hello"))
+}

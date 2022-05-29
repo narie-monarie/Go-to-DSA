307ee0b12b9c1d282f4f6207a17654bef1e29c76
narie <monarijob@yahoo.com>
2022-05-29 07:53:17 +0300

    stupid work

---
+++ Go Projects/Go Terminal/go.mod
@@ -0,0 +1,9 @@
+module github.com/narie-monarie
+
+go 1.18
+
+require (
+	github.com/apoorvam/goterminal v0.0.0-20180523175556-614d345c47e5 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)
---
+++ Go Projects/Go Terminal/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/apoorvam/goterminal"
+)
+
+func main() {
+	// get an instance of writer
+	writer := goterminal.New()
+
+	for i := 0; i < 100; i = i + 10 {
+		// add your text to writer's buffer
+		fmt.Fprintf(writer, "Downloading (%d/100) bytes...\n", i)
+		// write to terminal
+		writer.Print()
+		time.Sleep(time.Millisecond * 200)
+
+		// clear the text written by previous write, so that it can be re-written.
+		writer.Clear()
+
+	}
+
+	// reset the writer
+	writer.Reset()
+	fmt.Println("Download finished!")
+
+}

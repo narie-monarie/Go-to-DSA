8b118ef1e6a5e98ee7f939f242ffaccd2f544a6c
JOB MONARI <monarijob@yahoo.com>
2022-08-22 22:31:45 +0300

    Time: 232 ms (95.56%), Space: 57.9 MB (44.44%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cs
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cs
@@ -1,5 +1,5 @@
 public class Solution {
     public string KthLargestNumber(string[] nums, int k) {
-        return nums.OrderBy(x => x.Length).ThenBy(x => x).ElementAt(nums.Length-k);
+        return nums.OrderBy(A => A.Length).ThenBy(A => A).ElementAt(nums.Length-k);
     }
 }

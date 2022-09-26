8ec88e1d0feda35ac38f142292834cd66d0f4011
JOB MONARI <monarijob@yahoo.com>
2022-09-26 19:33:14 +0300

    Time: 28 ms (54.39%), Space: 11.3 MB (18.07%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int,int> m;
-        int k;
         for(int i = 0; i < nums.size(); i++){
-            k = target - nums[i];
-            if (m.find(k) != m.end()){
+            if (m.find(target - nums[i]) != m.end()){
                 return {i,m[target - nums[i]]};
             }
             m[nums[i]] = i;

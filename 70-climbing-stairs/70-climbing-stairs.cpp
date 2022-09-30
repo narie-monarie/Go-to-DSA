3428fb3b8f2676437614e6b2c8e55affcbda0dee
JOB MONARI <monarijob@yahoo.com>
2022-09-30 22:23:31 +0300

    Time: 0 ms (100.00%), Space: 5.9 MB (57.09%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2)
+           return n;
+        int cls[n];
+        cls[0] = 1;
+        cls[1] = 2;
+        for(int i=2; i<n; i++)
+            cls[i] = cls[i-1] + cls[i-2];
+        return cls[n-1];
+    }
+};

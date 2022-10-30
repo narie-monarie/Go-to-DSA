89f9b2068b419ce6318f9662ceceb7e6e8a13145
JOB MONARI <monarijob@yahoo.com>
2022-10-30 19:15:02 +0300

    Time: 0 ms (100.00%), Space: 2.6 MB (50.96%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.go
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.go
@@ -5,20 +5,25 @@
  *     Next *ListNode
  * }
  */
-func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-    if (l1 == nil) && (l2 == nil){
-        return nil
-    } else if l1 == nil{
-        return l2
-    } else if l2 == nil{
-        return l1
-    }
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+    result := &ListNode{}
+    current := result
+    for list1 != nil && list2 != nil {
+        if list1.Val < list2.Val {
+            current.Next = list1
+            list1 = list1.Next
+        } else {
+            current.Next = list2
+            list2 = list2.Next
+        }
     
-    if l2.Val > l1.Val{
-        l1.Next = mergeTwoLists(l1.Next, l2)
-        return l1
-    } else{
-        l2.Next = mergeTwoLists(l1, l2.Next)
-        return l2
+        current = current.Next
     }
+        if list1 == nil {
+            current.Next = list2
+        } else {
+            current.Next = list1
+        }
+    
+     return result.Next
 }

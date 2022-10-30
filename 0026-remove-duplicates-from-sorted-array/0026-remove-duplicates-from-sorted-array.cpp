33d8a4533247cbc7f7f7b71c60447bf8c42b163c
JOB MONARI <monarijob@yahoo.com>
2022-10-30 19:10:57 +0300

    Time: 30 ms (33.24%), Space: 18.3 MB (74.89%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,11 +3,11 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int insertIndex = 1;
         for(int i = 1; i < nums.size(); i++){
-        if(nums[i - 1] != nums[i]){  
-            nums[insertIndex] = nums[i];  
-            insertIndex++; 
+            if(nums[i - 1] != nums[i]){  
+                nums[insertIndex] = nums[i];  
+                insertIndex++; 
+            }
         }
-    }
-    return insertIndex;
+        return insertIndex;
     }
 };

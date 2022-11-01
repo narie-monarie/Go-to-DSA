6e984cd8e7f36550960a77d6418425e9b0a3f2fe
JOB MONARI <monarijob@yahoo.com>
2022-11-02 02:02:35 +0300

    Time: 12 ms (39.57%), Space: 47.9 MB (79.87%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -4,21 +4,25 @@ class MinStack {
     public MinStack() {
         this.stack = new Stack<>();
     }
-    
-public void push(int x) {
-    if(x<=min) {stack.push(min); min=x;}
-    stack.push(x);
-}
-public void pop() {
-    if(stack.peek()==min){ stack.pop(); min=stack.pop(); }
-    else stack.pop();
-}
-public int top() {
-    return stack.peek();
-}
-public int getMin() {
-    return min;
-}
+    public void push(int x) {
+        if(min>=x){
+            stack.push(min); 
+            min=x;
+        }
+        stack.push(x);
+    }
+    public void pop() {
+        if(stack.peek()==min){
+            stack.pop(); 
+            min=stack.pop(); 
+        }else stack.pop();
+    }
+    public int top() {
+        return stack.peek();
+    }
+    public int getMin() {
+        return min;
+    }
 }
 
 /**

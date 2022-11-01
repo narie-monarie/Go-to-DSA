efb7024cba37e26914e07fafe95ec31258fef2f3
JOB MONARI <monarijob@yahoo.com>
2022-11-02 02:01:06 +0300

    Time: 4 ms (97.63%), Space: 43.9 MB (96.20%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,29 +1,24 @@
 class MinStack {
     Stack<Integer> stack = new Stack<>();
-    
+    int min=Integer.MAX_VALUE;
     public MinStack() {
         this.stack = new Stack<>();
     }
     
-    public void push(int val) {
-        stack.push(val);
-    }
-    
-    public void pop() {
-        stack.pop();
-    }
-    
-    public int top() {
-        return stack.peek();
-    }
-    
-    public int getMin() {
-        int min = Integer.MAX_VALUE; 
-        for(int a : stack)
-            min = Math.min(min, a);
-        
-        return min;
-    }
+public void push(int x) {
+    if(x<=min) {stack.push(min); min=x;}
+    stack.push(x);
+}
+public void pop() {
+    if(stack.peek()==min){ stack.pop(); min=stack.pop(); }
+    else stack.pop();
+}
+public int top() {
+    return stack.peek();
+}
+public int getMin() {
+    return min;
+}
 }
 
 /**

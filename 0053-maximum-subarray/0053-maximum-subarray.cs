2e71417ab22b45bfc970f7db6e203850ea5f1ab8
JOB MONARI <monarijob@yahoo.com>
2022-11-13 21:00:35 +0300

    Time: 324 ms (73.88%), Space: 48.9 MB (75.05%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cs
+++ 0053-maximum-subarray/0053-maximum-subarray.cs
@@ -1,12 +1,14 @@
 public class Solution {
     public int MaxSubArray(int[] nums) {
-        int ans = int.MinValue;
-        int a = 0;
-        foreach(int x in nums){
-            a += x;
-            ans = Math.Max(a, ans);
-            a = Math.Max(a, 0);
+        int maxSub = nums[0];
+        int sum = 0;
+        for(int i = 0; i < nums.Length; i++){
+            if(sum < 0 )
+                sum = 0;
+            sum += nums[i];
+            maxSub = Math.Max(maxSub, sum);
         }
-        return ans;
+        
+        return maxSub;
     }
 }
